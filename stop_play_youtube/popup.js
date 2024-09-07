b10f990016682f42593957cd35e7c894ff6f8e8c
Cheroberous <102479391+Cheroberous@users.noreply.github.com>
2024-09-07 23:06:43 +0200

    Update popup.js
    new comm type

--- stop_play_youtube/popup.js
+++ stop_play_youtube/popup.js
@@ -1,66 +1,52 @@
 
-//console.log("popup opened");
 
-var x= chrome.runtime.sendMessage(
-        
-            {
-                dst:"lista",                                    
-                video_title: "x",
-                your_id:""
-            }, (response)=>{
-                //console.log("arrivata response, accedo dizionario");
-                // var reparsed= JSON.parse(response);
-                //console.log(response.info);
-                
-               
 
-                // if(response.info!=undefined){
+chrome.storage.session.get(["myDictionary"]).then( (result) => {
 
-                    // //console.log("not empty");
+    
+        let dic_risistemato= JSON.parse(result.myDictionary)
 
-                    var list1 = document.getElementById('lista_video_attivi');
-                    for (const [key, value] of Object.entries(response.info)) {
-                        //console.log("contenuto");
-                        //console.log(key, value);
+        var list1 = document.getElementById('lista_video_attivi');
+
+
+        for (const [key, value] of Object.entries(dic_risistemato)) {
+
             
                         
             
-                        //console.log("dovrei aggiungere lista elementi a html");
                             
-                        const newItem = document.createElement('li');
-                        newItem.className = key;                                      // do sia all'item che al button la classname id
-                        // Add content to the new item
-                        newItem.textContent =value;                            
+            const newItem = document.createElement('li');
+            newItem.className = key;                                   
+            newItem.textContent =value;                            
             
-                        const button = document.createElement('button');
-                        button.textContent = "play/pause"; // Set the button's text to the list item's text          metti una immagine??
-                        button.className = key;                                           // do sia all'item che al button la classname id
-                        button.classList.add("custom-button");
+            const button = document.createElement('button');
+            button.textContent = "play/pause"; 
+            button.className = key;                                          
+            button.classList.add("custom-button");
 
-                        button.onclick = function() {
-                            handleClick(button.className);
-                        };
-                        // Append the button to the list item
-                        newItem.appendChild(button);
-                        // Append the list item (with the button) to the list
-                        list1.appendChild(newItem);
+            button.onclick = function() {
+                handleClick(button.className);
+            };
+          
+            newItem.appendChild(button);
+         
+            list1.appendChild(newItem);
                         
                 
-                    }
-                // }
-
             }
-          );
+            
+         
+                
+        }); 
 
 
 
 
-function handleClick(id_tab){                                      //potrebbe anche mandarla lui al content
-    //console.log("vuoi chiudere video di tab: ",id_tab);
+function handleClick(id_tab){                                   
+ 
         
     var id_num= parseInt(id_tab);
-    
-        
+              
     chrome.tabs.sendMessage(id_num,
         
         {
