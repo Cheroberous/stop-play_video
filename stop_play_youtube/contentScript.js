6bb133d7e58b77c090ddadc1c70ba1ff2d852fe9
Cheroberous <102479391+Cheroberous@users.noreply.github.com>
2024-09-07 23:05:57 +0200

    Update contentScript.js

--- stop_play_youtube/contentScript.js
+++ stop_play_youtube/contentScript.js
@@ -1,4 +1,6 @@
 
+
+var debug=0;
 var x;
 
     function get_title() {
@@ -8,7 +10,7 @@ var x;
             const interval = setInterval(()=>{
                 const prendo_titolo = document.getElementsByClassName("style-scope ytd-watch-metadata")[1];
                 if(prendo_titolo){
-             
+               
                     clearInterval(interval)
                     resolve(prendo_titolo);
                 }
@@ -24,7 +26,6 @@ var x;
             const interval = setInterval(()=>{
                 const prendo_button = document.getElementsByClassName("ytp-play-button ytp-button")[0];
                 if(prendo_button){
-      
                     clearInterval(interval)
                     resolve(prendo_button);
                 }
@@ -35,32 +36,24 @@ var x;
     }
 
 
-    // riceve messaggio da bg quando è cambiato video youtube
     chrome.runtime.onMessage.addListener((obj, sender, response)=> { 
         
-        //console.log("arrivato un messaggio (cs)");
 
         if(obj.dst=="cs"){                  
-          
 
-            //console.log("arrivato messaggio per cs, controllo se sono su video youtube o pagina home");
             setTimeout(do_your_thing, 2000,obj.your_id); 
-      
 
         }
 
         if(obj.dst=="tab_sp"){
-            //console.log("dovrei stoppare il video nella mia scheda");
-            //console.log("bottone: ",pause_play);
+      
             pause_play.click();
         }
 
     });
 
-    
 
     function send_info_to_popup(titolo_video,my_id){
-        //console.log("dovrei mandare a popup == ",titolo_video);
         chrome.runtime.sendMessage(
         
             {
@@ -73,30 +66,20 @@ var x;
     }
 
 
+
+
 var pause_play;
 
 
     async function do_your_thing(page_id) {       
 
-        //console.log("rieseguo script per refresh/cambio video");
-
-
-
-
-        
-
-            //console.log('sono in un video, retriving title');
-
-            pause_play= await get_button();               
-            const titolo = await get_title();                 
-            
-
-            //console.log("titolo == ",titolo.outerText);             
+            pause_play= await get_button();                 
+            const titolo = await get_title();                    
+                     
 
             sessionStorage.setItem("titolo",titolo.outerText);   
             sessionStorage.setItem("bottone", pause_play);   
 
-            //console.log("RITORNATE ENTRABE");
 
             send_info_to_popup(titolo.outerText,page_id);
 
@@ -110,7 +93,7 @@ var pause_play;
 
     }
 
- 
+  
 
     
 
@@ -135,105 +118,3 @@ var pause_play;
 
 
 
-// async function do_things(){
-/*
-chrome.tabs.onUpdated.addListener((tabId, tab) =>{                // messaggio mandato circa 8000 mila volte
-
-    // const ident=tabId;          
-    //console.log("so che ha ricaricato");
-    //console.log("tab id: ",tabId);
-    // //console.log("dove sono  == ",tab.url);           //funziona per pagne normali (google)
-    // //console.log("dove sono  == ",tab.pendingUrl);
-    // //console.log("chi sono  == ",tab.title);
-
-    chrome.tabs.query({ active: true},(tab_ris)=>{                        // controlla se è un video di toutube e prendii il titolo
-
-        const id=tab_ris[0].id;
-        const new_url=tab_ris[0].url;                         // funziona se refresho youtube
-        const new_url1=tab_ris[0].pendingUrl;                 
-        const title=tab_ris[0].title;                 
-
-
-        //if(new_url.match(match_video) && new_url!=attiva){
-
-            // var ele = document.querySelector('#title.style-scope.ytd-watch-metadata');
-
-            attiva=new_url;
-
-            //console.log("tab  == ",tab_ris[0].id);
-            //console.log("dove sono andato == ",new_url);
-            // //console.log("dove sono andato1 == ",new_url1);
-            //console.log("chi sono sono1 == ",tab_ris[0].title);  // sembra prenderlo
-
-
-            
-
-            chrome.runtime.sendMessage({
-
-                tab_id: id,
-                tab_title: title,
-                tab_url: new_url
-
-                }
-            );
-
-        //}
-
-        
-
-    });          
-    
-
-    
-    // if (tab.url && tab.url.includes("youtube.com/watch")){
-
-    //     const queryParameters=  tab.url.split("?")[1];
-    //     const urlParameters = new URLSearchParams(queryParameters);
-
-    //     //console.log(urlParameters);
-
-    //     chrome.tabs.sendMessage(tabId, {
-
-    //         type: "NEW",
-    //         videold: urlParameters.get("v")
-
-    //     });
-    // }
-});
-
-
-
-
-
-*/
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    // chrome.runtime.onMessage.addListener((obj, sender, response)=> {           // ricevere da background
-
-    //     const {type,value,videoId } = obj;
-
-       
-    //     //console.log("ricevuto messaggio da background");
-
-    //     setTimeout(function() {
-    //         var x= document.getElementsByClassName("ytp-play-button ytp-button");
-    //         // //console.log("trovato == ",x);
-    //         // x[0].click();
-    //         //console.log("ho clickato");
-    //       }, 6000);
-        
-
-    // });
