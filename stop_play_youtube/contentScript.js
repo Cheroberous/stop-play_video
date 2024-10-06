eb879ac825c14b7bf9e7dda2e6b9667f51884c34
Cheroberous <102479391+Cheroberous@users.noreply.github.com>
2024-10-06 17:21:44 +0200

    Update contentScript.js

--- stop_play_youtube/contentScript.js
+++ stop_play_youtube/contentScript.js
@@ -2,30 +2,40 @@
 
 var debug=1;
 var x;
+var pause_play;
+var forward;
+var back;
+var data;
+var controller;
+var me;
 
 
-    function get_title() {
 
-        return new Promise((resolve)=>{
 
-            const interval = setInterval(()=>{
-                const prendo_titolo = document.getElementsByClassName("style-scope ytd-watch-metadata")[1];
-                if(prendo_titolo){
-       
-                    clearInterval(interval)
-                    resolve(prendo_titolo);
-                }
-            }, 500);
+    function get_data(action) {
 
-        });
-
+    var string;
+    var pos=0;
+    if(action==0){
+        string="ytp-next-button ytp-button";                   
     }
-    function get_button() {
-
+    if(action==1){
+        string="video-stream html5-main-video";                 
+    }
+    if(action==2){
+        string="style-scope ytd-watch-metadata";               
+        pos=1;
+    }
+    if(action==3){
+        string="ytp-play-button ytp-button";                   
+    }
+    if(action==4){
+        string="video-stream";
+    }
         return new Promise((resolve)=>{
 
             const interval = setInterval(()=>{
-                const prendo_button = document.getElementsByClassName("ytp-play-button ytp-button")[0];
+                const prendo_button = document.getElementsByClassName(string)[pos];
                 if(prendo_button){
                     clearInterval(interval)
                     resolve(prendo_button);
@@ -33,47 +43,79 @@ var x;
             }, 500);
 
         });
+    
 
-    }
-    function get_f_button() {
+    
+        
+    
 
-        return new Promise((resolve)=>{
-
-            const interval = setInterval(()=>{
-                const prendo_button = document.getElementsByClassName("ytp-next-button ytp-button")[0];
-                if(prendo_button){
-                    clearInterval(interval)
-                    resolve(prendo_button);
-                }
-            }, 500);
-
-        });
 
     }
 
-    chrome.runtime.onMessage.addListener((obj, sender, response)=> { 
+
+
+
+
+
+
+
+chrome.runtime.onMessage.addListener((obj, sender, sendResponse)=> { 
         
    
-        if(obj.dst=="cs"){                 
-     
+        if(obj.dst=="cs"){                   
+        
             setTimeout(do_your_thing, 2000,obj.your_id,obj.video_title); 
+            return false;
 
         }
 
-        if(obj.dst=="tab_sp"){
+        if(obj.dst=="time"){
+            controller.currentTime=obj.info;
+            return false;
+        }
 
+        if(obj.dst=="tab_sp"){
+        
             pause_play.click();
+            return false;
+
         }
         if(obj.dst=="f"){
 
             forward.click();
+            return false;
+
 
         }
-     
+
+
+        if(obj.dst=="info"){
+            var data1=get_data(1).then((result)=>{
+
+
+                var x1=result.duration;
+                var y=result.currentTime;
+
+                sendResponse({
+                    max: x1,
+                    cur: y
+
+                });
+
+
+            });
+        
+
+       
+
+
+        return true;              
+        
+        }
+    
 
     });
 
-  
 
     function send_info_to_popup(titolo_video,my_id,thumbnail){
         chrome.runtime.sendMessage(
@@ -88,50 +130,55 @@ var x;
           );
     }
 
- 
 
 
-var pause_play;
-var forward;
-var back;
-var me;
+
+
 
 
     async function do_your_thing(page_id,thumbnail) {       
 
-
+     
             me=page_id;
 
-            pause_play= await get_button();                 
-            const titolo = await get_title();                    
-            forward= await get_f_button();
-                  
 
-            sessionStorage.setItem("titolo",titolo.outerText);   
-            sessionStorage.setItem("bottone", pause_play);   
+            pause_play= await get_data(3);                 
+            const titolo = await get_data(2);                      
+            forward= await get_data(0);
+            controller= await get_data(4);
+
 
 
             send_info_to_popup(titolo.outerText,page_id,thumbnail);
 
+         
 
     }
 
+
+
+
+
+
+  
     document.addEventListener('click', function(event) {
 
         if (event.isTrusted) {
+        
 
-            var x=event.target.className;                       
+            var x=event.target.className;                      
+            console.log("classe: ",x);
 
             if (x!=undefined && (x=='ytp-play-button ytp-button' || x=="video-stream html5-main-video")) {
 
-                    send_msg();
+                send_msg();
             }
         
         }
+       
  
     });
 
-   
     document.addEventListener('keydown', function(event) {
         if (event.code === 'Space' || event.key === ' ') {
             send_msg();
@@ -139,17 +186,32 @@ var me;
         }
     });
 
-    function send_msg(){
 
-        if(me!=undefined){
-    
-            chrome.runtime.sendMessage(
-    
-                {
-                dst: "1_bg",
-                info: me
-                }
-            
-            );
-        }
+  
+
+
+function send_msg(){
+    if(me!=undefined){
+
+        chrome.runtime.sendMessage(
+
+            {
+            dst: "1_bg",
+            info: me,
+            action: 1
+            }
+        
+        );
     }
+}
+
+
+
+    
+
+
+  
+
+
+
+
